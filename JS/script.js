b004df22f5bda94b452da50ac48adf7e06830ac5
MARCEDO-12 <naktaxi080@gmail.com>
2026-03-28 15:08:35 +0700

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -9,3 +9,7 @@ registerBtn.addEventListener("click", () => {
 loginBtn.addEventListener("click", () => {
     container.classList.remove("active");
 });
+// ឆែកមើលថាបាន Login ឬនៅ? បើមិនទាន់ទេ បណ្តេញទៅកាន់ទំព័រ Login វិញ
+if (localStorage.getItem("isLoggedIn") !== "true") {
+    window.location.href = "../index.html"; 
+}
